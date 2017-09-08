7a5da2f821d5f25d8f6a55799b2e075723a8e550
Gitu Mbugua <gmbugua38@gmail.com>
2017-09-08 23:33:00 +0300

    change console log to display response in html

--- js/search.js
+++ js/search.js
@@ -5,7 +5,7 @@ function GithubSearch() {
 
 }
 
-GithubSearch.prototype.getRepos = function(userName, displayName) {
+GithubSearch.prototype.getRepos = function(userName, displayName, displayRepo, description) {
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
      displayName(response.name);
    }).fail(function(error){
@@ -14,8 +14,8 @@ GithubSearch.prototype.getRepos = function(userName, displayName) {
 // user name
    $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
      for (var index = 0; index <= response.length; index++ ) {
-       console.log(response[index].name);
-       console.log(response[index].description);
+       displayRepo(response[index].name);
+       description(response[index].description);
      }
    });
 };

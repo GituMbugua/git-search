0e6f99540e9a6f8ba54f90867c5a09f94f01de94
Gitu Mbugua <gmbugua38@gmail.com>
2017-09-08 18:12:45 +0300

    remove console log; display to interfacefile

--- js/search.js
+++ js/search.js
@@ -5,12 +5,20 @@ function GithubSearch() {
 
 }
 
-GithubSearch.prototype.getRepos = function(userName) {
+GithubSearch.prototype.getRepos = function(userName, displayName) {
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
-     console.log(response);
+     displayName(response.name);
    }).fail(function(error){
      console.log(error.responseJSON.message);
    });
+// user name
+   $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
+     for (var index = 0; index <= response.length; index++ ) {
+       console.log(response[index].name);
+       console.log(response[index].description);
+     }
+   });
 };
+
 
 exports.searchModule = GithubSearch;

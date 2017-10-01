8dad621a435a4181de27c43c0f8e79b4686a94b2
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-01 19:14:03 +0300

    clear divs and change description info

--- build/js/app.js
+++ build/js/app.js
@@ -31,16 +31,20 @@ exports.searchModule = GithubSearch;
 var GithubSearch = require('./../js/search.js').searchModule;
 var displayName = function(name) {
     if (name === null) {
-      $('#showAccount').append("<h3>This account does not have a username.</h3>");
+      $('#login').append("<h3>This account does not have a username.</h3>");
     } else {
-    $('#showAccount').append("<h3>Username: </h3><h4>" + name + "</h4>");
+    $('#login').append("<h3>Username:" + ' ' + name + "</h3>");
   }
 };
 var displayRepo = function(repo) {
-  $('#showAccount').append('<h4>' + repo + '</h4>');
+  $('#showAccount').append('<h4><strong>' + repo + '</strong></h4>');
 };
 var description = function(descr) {
-  $('#showAccount').append('<p>' + descr + '</p>');
+    if (descr === null) {
+      $('#showAccount').append('<p>No Description</p>');
+    } else {
+      $('#showAccount').append('<p>Description:' + ' ' + descr + '</p>');
+    }
 };
 
 $(document).ready(function() {
@@ -48,8 +52,8 @@ $(document).ready(function() {
 
   $('#form').submit(function(event) {
     event.preventDefault();
-
-    $('#showAccount').val("");
+    $('#login').text(" ");
+    $('#showAccount').text(" ");
     var account = $('#userInput').val();
 
     currentGithubSearch.getRepos(account, displayName, displayRepo, description);
